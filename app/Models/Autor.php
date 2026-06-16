f8f09795ab392aa610b6453ab5cd1932f95ef6d9
Gabriel29candido <134733503+Gabriel29candido@users.noreply.github.com>
2026-06-16 23:39:02 +0000

    fix: adiciona HasFactory nos models e corrige factories

--- app/Models/Autor.php
+++ app/Models/Autor.php
@@ -1,20 +1,15 @@
 <?php
-
 namespace App\Models;
-
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 class Autor extends Model
 {
-    //
+    use HasFactory;
     protected $fillable = [
         'nome',
-        'sobrenome',
         'nacionalidade',
     ];
-
     protected $table = 'autores';
-
     public function livros()
     {
         return $this->hasMany(Livro::class, 'autor_id');
